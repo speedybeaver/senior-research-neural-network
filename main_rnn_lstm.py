12c5caf4445e98d6d7c82111e4f9236000dc3cb8
Gio Mandwee <gmandwee@gmail.com>
2024-10-22 23:05:43 -0400

    Update main_rnn_lstm.py

--- main_rnn_lstm.py
+++ main_rnn_lstm.py
@@ -1,10 +1,16 @@
 import torch
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt # data visualization (e.g plot graphs)
 from sklearn.model_selection import train_test_split
 from torch import optim  # For optimizers like SGD, Adam, etc.
 from torch import nn  # All neural network modules
-import tqdm
+from tqdm import ( tqdm )
+import kagglehub
+from torch.utils.data import (
+    DataLoader,
+    TensorDataset
+)  # Gives easier dataset managment by creating mini batches etc.
 
 # Set device
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -14,8 +20,8 @@ input_size = 80
 hidden_size = 256
 num_layers = 2
 num_classes = 7
-learning_rate = 0.002
-num_epochs = 1
+learning_rate = 0.001
+num_epochs = 2
 
 # creating rnn lstm model
 class LSTM(nn.Module):
@@ -27,38 +33,48 @@ class LSTM(nn.Module):
         self.fc = nn.Linear(hidden_size, num_classes)
 
     def forward(self, x):
+
         # Set initial hidden and cell states
-        hidden_states = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
-        cell_states = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+        batch_size = x.size(0)
+        hidden_states = torch.zeros(self.num_layers, self.hidden_size).to(device)
+        cell_states = torch.zeros(self.num_layers, self.hidden_size).to(device)
 
         # Forward propagate LSTM
         out, _ = self.lstm(x, (hidden_states, cell_states))
-        out = out.reshape(out.shape[0], -1)
-
+    
         # Decode the hidden state of the last time step
         out = self.fc(out)
-        return out, x
+        return out
     
 torch.manual_seed(41)
-model = LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, num_classes=num_classes)
+model = LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, num_classes=num_classes).to(device)
 
 # getting path and reading the csv file for ALL inputs
-path = r"C:\Users\gmand\.cache\kagglehub\datasets\nccvector\electromyography-emg-dataset\versions\1\Electro-Myography-EMG-Dataset\extracted_features_and_labeled_dataset(easiest to work with)\emg_all_features_labeled.csv"
+path = kagglehub.dataset_download("nccvector/electromyography-emg-dataset")
+path = path + r"\Electro-Myography-EMG-Dataset\extracted_features_and_labeled_dataset(easiest to work with)\emg_all_features_labeled.csv"
 df = pd.read_csv(path)
 
 # preparing data for training
 
-x = df.drop("1.2", axis=1) # training data
+X = df.drop("1.2", axis=1) # training data
 y = df["1.2"] # correct classifcations
 
 # splitting up the data set
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=41)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=41)
 
-x_train = torch.tensor(x_train.values)
-x_test = torch.tensor(x_test.values)
 
-y_train = torch.LongTensor(y_train.values)
-y_test = torch.LongTensor(y_test.values)
+X_train = torch.tensor(X_train.values).float().to(device)
+X_test = torch.tensor(X_test.values).float().to(device)
+
+y_train = torch.LongTensor(y_train.values).to(device)-1
+y_test = torch.LongTensor(y_test.values).to(device)-1
+
+# intializing them as torch datasets
+train_dataset = TensorDataset(X_train, y_train)
+test_dataset = TensorDataset(X_test, y_test)
+
+train_data = DataLoader(train_dataset, batch_size=1)
+test_data = DataLoader(test_dataset, batch_size=1)
 
 # set criterion, optimizer, and learning rate
 criterion = nn.CrossEntropyLoss()
@@ -66,26 +82,40 @@ optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
 # training the network
 
-losses = []
 
 def train():
+    losses = []
     for epoch in range(num_epochs):
-        for data, targets in zip(x_train, y_train):
+        for batch, (data, targets) in enumerate(tqdm(train_data)):
             # Get data to cuda if possible
-            data = data.to(device=device).squeeze(1)
+            data = data.to(device=device)
             targets = targets.to(device=device)
-
+            
             # forward
             scores = model(data)
-            loss = criterion(scores, targets)
-            losses.append(loss.detach().numpy())
 
-            # backward
+            loss = criterion(scores, targets)
+            if batch%100 == 0:
+                losses.append(loss.cpu().detach().numpy())
+
+            # data
             optimizer.zero_grad()
             loss.backward()
-
-            # gradient descent update step/adam step
             optimizer.step()
-    print(losses)
-        
+
+def test_accuracy():
+    with torch.no_grad():
+        for batch, (data, targets) in enumerate(tqdm(train_data)):
+            # Get data to cuda if possible
+            data = data.to(device=device)
+            targets = targets.to(device=device)
+            
+            # forward
+            scores = model(data)
+
+            loss = criterion(scores, targets)
+            if batch%100 == 0:
+                losses.append(loss.cpu().detach().numpy())
+
 train()
+test_accuracy()
